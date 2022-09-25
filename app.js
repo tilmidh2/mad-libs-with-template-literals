be2a3355a995ddad678ac5c3fd6e7215aa8a9559
David Booth-Metwally <davidmbmetwally@gmail.com>
2022-09-25 16:10:35 -0700

    completed assignment

---
+++ app.js
@@ -0,0 +1,74 @@
+
+
+
+// Assign STRING VALUES ONLY to all of the following variables.
+// Assign an adjective
+let adj1;
+adj1 = 'agile';
+// Assign an adjective
+let adj2;
+adj2 = 'bulky';
+// Assign an adjective
+let adj3;
+adj3 = 'fast';
+// Assign an adjective
+let adj4;
+adj4 = 'stalky';
+// Assign an adjective
+let adj5;
+adj5 = 'booming';
+// Assign an adjective
+let adj6;
+adj6 = 'subsiding';
+// Assign an adverb
+let adverb;
+adverb = 'swiftly';
+// Assign a color
+let color;
+color = 'green';
+// Assign a noun
+let noun1;
+noun1 = 'competitor';
+// Assign a noun
+let noun2;
+noun2 = 'strike';
+// Assign a noun
+let noun3;
+noun3 = 'haymaker';
+// Assign a noun
+let noun4;
+noun4 = 'jab';
+// Assign a noun
+let noun5;
+noun5 = 'traveler'; 
+// Assign a noun
+let noun6;
+noun6 = 'legend';
+// Assign a number
+let num;
+num = 18;
+// Assign a plural noun
+let pnoun1;
+pnoun1 = 'folks';
+
+// Open your console to see the results!
+// SPOILER ALERT: KEEP SCROLLING TO VIEW THE REST OF THE CODE
+let string1 = ''
+let string2 = ""
+let ssj3 = 'goku'
+let string3 = `${ssj3}`
+
+//Using template literals to plug in the values
+let story = `It has often been said that a dog is a man's best ${noun1}. Dogs are very ${adj1} and can be taught many ${adj2}
+tricks. A dog can be trained to carry a ${noun2} in his mouth.
+If you throw his ${noun3}, he will run and fetch it. Dogs
+will also bark ${adverb} if someone tries to break into your
+${noun4} during the night. One of the most popular canine pets
+today is the ${noun5} Spaniel. Spaniels have curly ${color}
+coats and ${adj3} ears. They also have very ${adj4}
+dispositions and live to be ${num} years old. Other popular dogs
+are ${adj5} Terriers, German ${pnoun1}, and the
+${adj6} Poodle. Every ${noun6} should have a loyal dog. ${string3}`;
+
+//Console log the completed story
+console.log(story);
---
+++ readme.md
@@ -0,0 +1,11 @@
+Steps to Completing Take Home Day 2 , Week 1
+
+1. Create folder containing index.html & script.js
+
+2. link script.js to index.html
+
+3. paste script from codepen.io
+
+4. assign string (and 1 number) values to variables
+
+5. save it and launch it to a server and view the console
